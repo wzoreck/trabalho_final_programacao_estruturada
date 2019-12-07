1a3ff4388d36003eec3106d12b32a5fce7c712ce
Daniel Wzoreck <wzoreck2000@gmail.com>
2019-12-07 18:33:20 -0300

    programa funcionando corretamente, com funções de cadastro, consulta, busca por ID, ID automático, atualização de registros existentes

--- final.c
+++ final.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Estruturas
 struct videogame {
@@ -42,15 +43,15 @@ int listar_venda();
 int alterar_videogame(struct videogame v);
 int alterar_jogo(struct jogo j);
 int alterar_pessoa(struct pessoa p);
-int alterar_venda(struct venda v);
+int alterar_venda(struct venda v, struct venda w);
 
 int videogame_id_automatico();
 int jogo_id_automatico();
 int pessoa_id_automatico();
 
-int videogame_busca_por_referencia(int id);
-int jogo_busca_por_referencia(int id);
-int pessoa_busca_por_referencia(int id);
+int videogame_busca_por_referencia(int id, int modo); // "modo" serve para controlar se a função irá printar os valores ou não
+int jogo_busca_por_referencia(int id, int modo);
+int pessoa_busca_por_referencia(int id, int modo);
 int venda_busca_por_referencia(int id_pessoa);
 
 int main() {
@@ -59,6 +60,7 @@ int main() {
     struct jogo jogo;
     struct pessoa pessoa;
     struct venda venda;
+    struct venda venda2;
 
     int escolha1, escolha2, retorno, loop=0, limpar_tela=0, busca_id;
 
@@ -98,8 +100,6 @@ int main() {
                         retorno = cadastrar_videogame(videogame);
                         if(retorno == 1) {
                             printf("Falha ao abrir o arquivo");
-                        } else if(retorno == 0) {
-                            retorno = 0;
                         }
                     break;
 
@@ -112,9 +112,9 @@ int main() {
                         fgets(jogo.nome, 50, stdin);
                         retorno = cadastrar_jogo(jogo);
                         if(retorno == 1) {
-                            printf("Falha ao abrir o arquivo");
-                        } else if(retorno == 0) {
-                            retorno = 0;
+                            printf("\nFalha ao abrir o arquivo");
+                        } else if(retorno == 2) {
+                            printf("\nNão á videogame cadastrado referente ao ID informado!");
                         }
                     break;
 
@@ -139,8 +139,6 @@ int main() {
                         retorno = cadastrar_pessoa(pessoa);
                         if(retorno == 1) {
                             printf("Falha ao abrir o arquivo");
-                        } else if(retorno == 0) {
-                            retorno = 0;
                         }
                     break;
 
@@ -155,8 +153,8 @@ int main() {
                         retorno = cadastrar_venda(venda);
                         if(retorno == 1) {
                             printf("Falha ao abrir o arquivo");
-                        } else if(retorno == 0) {
-                            retorno = 0;
+                        } else if(retorno == 2) {
+                            printf("\nNão há cadastro referente a um ID informado!");
                         }
                     break;
                 }
@@ -217,7 +215,7 @@ int main() {
                     case 5:
                         printf("\nInforme o id do vídeogame: ");
                         scanf("%d", &busca_id);
-                        retorno = videogame_busca_por_referencia(busca_id);
+                        retorno = videogame_busca_por_referencia(busca_id, 1);
                         if(retorno == 1) {
                             printf("\nNão há registro referente a esse ID!");
                         }
@@ -226,7 +224,7 @@ int main() {
                     case 6:
                         printf("\nInforme o id do jogo: ");
                         scanf("%d", &busca_id);
-                        retorno = jogo_busca_por_referencia(busca_id);
+                        retorno = jogo_busca_por_referencia(busca_id, 1);
                         if(retorno == 1) {
                             printf("\nNão há registro referente a esse ID!");
                         }
@@ -235,7 +233,7 @@ int main() {
                     case 7:
                         printf("\nInforme o id da pessoa: ");
                         scanf("%d", &busca_id);
-                        retorno = pessoa_busca_por_referencia(busca_id);
+                        retorno = pessoa_busca_por_referencia(busca_id, 1);
                         if(retorno == 1) {
                             printf("\nNão há registro referente a esse ID!");
                         }
@@ -255,6 +253,90 @@ int main() {
             case 3:  // ALTERAÇÕES
                 system("clear");
                 printf(" █████╗ ████████╗██╗   ██╗ █████╗ ██╗     ██╗███████╗ █████╗ ██████╗\n██╔══██╗╚══██╔══╝██║   ██║██╔══██╗██║     ██║╚══███╔╝██╔══██╗██╔══██╗\n███████║   ██║   ██║   ██║███████║██║     ██║  ███╔╝ ███████║██████╔╝\n██╔══██║   ██║   ██║   ██║██╔══██║██║     ██║ ███╔╝  ██╔══██║██╔══██╗\n██║  ██║   ██║   ╚██████╔╝██║  ██║███████╗██║███████╗██║  ██║██║  ██║\n╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝\n\n");
+                printf("\nEscolha a operação que deseja realizar!");
+                printf("\n\n[1] - Atualizar Registro de Videogame");
+                printf("\n[2] - Atualizar Registro de Jogo");
+                printf("\n[3] - Atualizar Registro de Cliente");
+                printf("\n[4] - Atualizar Registro de Venda");
+                printf("\n\nInforme sua escolha: ");
+                scanf("%d", &escolha2);
+                switch (escolha2) {
+                    case 1:
+                        printf("\nInforme o ID do videogame que deseja alterar: ");
+                        scanf("%d", &videogame.id);
+                        printf("Informe o novo nome do videogame: ");
+                        setbuf(stdin, NULL);
+                        fgets(videogame.nome, 50, stdin);
+                        printf("Informe a nova marca do videogame: ");
+                        setbuf(stdin, NULL);
+                        fgets(videogame.marca, 50, stdin);
+                        retorno = alterar_videogame(videogame);
+                        if(retorno == 1) {
+                            printf("\nNão há registro referente a esse ID!");
+                        }
+                    break;
+
+                    case 2:
+                        printf("\nInforme o ID do jogo que deseja alterar: ");
+                        scanf("%d", &jogo.id);
+                        printf("Informe o novo nome do jogo: ");
+                        setbuf(stdin, NULL);
+                        fgets(jogo.nome, 50, stdin);
+                        printf("Informe o ID do videogame que o jogo pertence: ");
+                        scanf("%d", &jogo.videogame_id);
+                        retorno = alterar_jogo(jogo);
+                        if(retorno == 1) {
+                            printf("\nNão há registro referente a esse ID!");
+                        }
+                    break;
+
+                    case 3:
+                        printf("\nInforme o ID do cliente que deseja alterar: ");
+                        scanf("%d", &pessoa.id);
+                        printf("Informe o primeiro nome do cliente: ");
+                        setbuf(stdin, NULL);
+                        fgets(pessoa.p_nome, 50, stdin);
+                        printf("Informe o sobrenome do cliente: ");
+                        setbuf(stdin, NULL);
+                        fgets(pessoa.u_nome, 50, stdin);
+                        printf("Informe o cpf do cliente (somente números): ");
+                        setbuf(stdin, NULL);
+                        fgets(pessoa.cpf, 13, stdin);
+                        printf("Informe de forma numérica o mês de nascimento do cliente: ");
+                        scanf("%d", &pessoa.mes_nasc);
+                        printf("Informe de forma numérica o ano de nascimento do cliente: ");
+                        scanf("%d", &pessoa.ano_nasc);
+                        printf("Informe o sexo do cliente\n[1] - Masculino\n[2] - Feminino\n[3] - Outro\nInforme o sexo: ");
+                        scanf("%d", &pessoa.sexo);
+                        retorno = alterar_pessoa(pessoa);
+                        if(retorno == 1) {
+                            printf("\nNão há registro referente a esse ID! retorno: %d", retorno);
+                        }
+                    break;
+
+                    case 4:
+                        printf("\nInforme o id da pessoa: ");
+                        scanf("%d", &venda.pessoa_id);
+                        printf("Informe o id do jogo: ");
+                        scanf("%d", &venda.jogo_id);
+                        printf("Informe a data (AAAA/MM/DD):");
+                        setbuf(stdin, NULL);
+                        fgets(venda.data, 12, stdin);
+
+                        printf("\nAtualizar registro");
+                        printf("\nInforme o id da pessoa: ");
+                        scanf("%d", &venda2.pessoa_id);
+                        printf("Informe o id do jogo: ");
+                        scanf("%d", &venda2.jogo_id);
+                        printf("Informe a data (AAAA/MM/DD):");
+                        setbuf(stdin, NULL);
+                        fgets(venda2.data, 12, stdin);
+                        retorno = alterar_venda(venda, venda2);
+                        if(retorno == 1) {
+                            printf("\nNão há registro referente aos dados informados");
+                        }
+                    break;
+                }
             break;
         }
         printf("\n\nDigite [1] para voltar ao menu inicial: ");
@@ -268,7 +350,7 @@ int main() {
 }
 
 // FUNÇÕES DE CADASTRO
-int cadastrar_videogame(struct videogame v) {
+int cadastrar_videogame(struct videogame v) { // OK
     FILE *f = fopen("arquivos/videogame.bin", "ab");
     if(f == NULL) {
         return 1;
@@ -277,17 +359,22 @@ int cadastrar_videogame(struct videogame v) {
     fclose(f);
     return 0;
 }
-int cadastrar_jogo(struct jogo j) {
+int cadastrar_jogo(struct jogo j) { // OK
     FILE *f = fopen("arquivos/jogo.bin", "ab");
     if(f == NULL) {
         return 1;
     }
-    // ADICIONAR Pesquisar se o videogame informado existe
-    fwrite(&j, sizeof(struct jogo), 1, f);
-    fclose(f);
-    return 0;
+    // Verifica se exeiste um registro referente ao ID do videogame informado
+    int retorno = videogame_busca_por_referencia(j.videogame_id, 0);
+    if(retorno == 0) {
+        fwrite(&j, sizeof(struct jogo), 1, f);
+        fclose(f);
+        return 0;
+    } else {
+        return 2;
+    }
 }
-int cadastrar_pessoa(struct pessoa p) {
+int cadastrar_pessoa(struct pessoa p) { // OK
     FILE *f = fopen("arquivos/pessoa.bin", "ab");
     if(f == NULL) {
         return 1;
@@ -296,19 +383,30 @@ int cadastrar_pessoa(struct pessoa p) {
     fclose(f);
     return 0;
 }
-int cadastrar_venda(struct venda v) {
+int cadastrar_venda(struct venda v) { // OK
     FILE *f = fopen("arquivos/venda.bin", "ab");
     if(f == NULL) {
         return 1;
     }
-    // ADICIONAR pesquisar se jogo e cliente existem
-    fwrite(&v, sizeof(struct venda), 1, f);
-    fclose(f);
-    return 0;
+    // Verifica se existe registro referente ao ID do jogo e ao ID do cliente informado
+    int retorno, y=1;
+    retorno = jogo_busca_por_referencia(v.jogo_id, 0);
+    if(retorno == 0)
+        y=0;
+    retorno = pessoa_busca_por_referencia(v.pessoa_id, 0);
+    if(retorno != 0)
+        y=1;
+    if(y == 0) {
+        fwrite(&v, sizeof(struct venda), 1, f);
+        fclose(f);
+        return 0;
+    } else {
+        return 2;
+    }
 }
 
 // FUNÇÕES DE CONSULTA
-int listar_videogame() {
+int listar_videogame() { // OK
     struct videogame v;
     FILE *f = fopen("arquivos/videogame.bin", "rb");
     if(f == NULL) {
@@ -323,7 +421,7 @@ int listar_videogame() {
     fclose(f);
     return x;
 }
-int listar_jogo() { // CORRETA
+int listar_jogo() { // OK
     struct jogo j;
     struct videogame v;
     FILE *fv = fopen("arquivos/videogame.bin", "rb");
@@ -347,7 +445,7 @@ int listar_jogo() { // CORRETA
     fclose(fv);
     return x;
 }
-int listar_pessoa() {
+int listar_pessoa() { // OK
     struct pessoa p;
     FILE *f = fopen("arquivos/pessoa.bin", "rb");
     if(f == NULL) {
@@ -362,7 +460,7 @@ int listar_pessoa() {
     fclose(f);
     return x;
 }
-int listar_venda() { /// ARRUMAR adiconar a concatenação
+int listar_venda() { // OK
     struct venda v;
     struct jogo j;
     struct pessoa p;
@@ -373,12 +471,22 @@ int listar_venda() { /// ARRUMAR adiconar a concatenação
         return 1;
     }
     int x=2;
-    // PRECISA ARRUMAR A BUSCA NÂO ESTÁ FILTRANDO CORRETAMENTE
+    // Encontrar o nome do CLIENTE referante ao ID e o nome do JOGO referente ao ID, e apresenta os dados
     while(fread(&v, sizeof(struct venda), 1, f)) {
-        fread(&p, sizeof(struct pessoa), 1, fp);
-        fread(&j, sizeof(struct jogo), 1, fj);
-        printf("\nCliente: %sJogo: %sData da venda: %s", p.p_nome, j.nome, v.data);
-        x=0;
+        fseek(fp, 0*sizeof(struct venda), 0);
+        while(fread(&p, sizeof(struct pessoa), 1, fp)) {
+            if(v.pessoa_id == p.id) {
+                fseek(fj, 0*sizeof(struct jogo), 0);
+                while(fread(&j, sizeof(struct jogo), 1, fj)) {
+                    if(v.jogo_id == j.id) {
+                        printf("\nCliente: %sJogo: %sData da venda: %s", p.p_nome, j.nome, v.data);
+                        break;
+                    }
+                }
+                break;
+            }
+            x=0;
+        }
     }
 
     fclose(f);
@@ -425,14 +533,15 @@ int pessoa_id_automatico() { // OK
     }
 }
 
-// BUSCAS POR REFERÊNCIA DE ID
-int videogame_busca_por_referencia(int id) { // OK
+//  FUNÇÕES DE BUSCA POR REFERÊNCIA - ID
+int videogame_busca_por_referencia(int id, int modo) { // OK
     struct videogame v;
     FILE *f = fopen("arquivos/videogame.bin", "rb");
     int x=1;
     while(fread(&v, sizeof(struct videogame), 1, f)) {
         if(v.id == id) {
-            printf("\nID: %d\nNome: %sMarca: %s\n", v.id, v.nome, v.marca);
+            if(modo == 1)
+                printf("\nID: %d\nNome: %sMarca: %s\n", v.id, v.nome, v.marca);
             x=0;
         }
     }
@@ -440,13 +549,14 @@ int videogame_busca_por_referencia(int id) { // OK
     fclose(f);
     return x;
 }
-int jogo_busca_por_referencia(int id) { // OK
+int jogo_busca_por_referencia(int id, int modo) { // OK
     struct jogo j;
     FILE *f = fopen("arquivos/jogo.bin", "rb");
     int x=1;
     while(fread(&j, sizeof(struct jogo), 1, f)) {
         if(j.id == id) {
-            printf("\nID: %d\nID Videogame: %d\nNome: %s", j.id, j.videogame_id, j.nome);
+            if(modo == 1)
+                printf("\nID: %d\nID Videogame: %d\nNome: %s", j.id, j.videogame_id, j.nome);
             x=0;
         }
     }
@@ -455,13 +565,14 @@ int jogo_busca_por_referencia(int id) { // OK
     return x;
 }
 
-int pessoa_busca_por_referencia(int id) { // OK
+int pessoa_busca_por_referencia(int id, int modo) { // OK
     struct pessoa p;
     FILE *f = fopen("arquivos/pessoa.bin", "rb");
     int x=1;
     while(fread(&p, sizeof(struct pessoa), 1, f)) {
         if(p.id == id) {
-            printf("\nID: %d\nNome: %sSobrenome: %sCPF: %sMês de nascimento: %d\nAno de nascimento: %d\nSexo: %d\n", p.id, p.p_nome, p.u_nome, p.cpf, p.mes_nasc, p.ano_nasc, p.sexo);
+            if(modo == 1)
+                printf("\nID: %d\nNome: %sSobrenome: %sCPF: %sMês de nascimento: %d\nAno de nascimento: %d\nSexo: %d\n", p.id, p.p_nome, p.u_nome, p.cpf, p.mes_nasc, p.ano_nasc, p.sexo);
             x=0;
         }
     }
@@ -477,6 +588,73 @@ int venda_busca_por_referencia(int id_pessoa) { // OK
         if(v.pessoa_id == id_pessoa) {
             printf("\nID Cliente: %d\nID Jogo: %d\nData da venda: %s", v.pessoa_id, v.jogo_id, v.data);
             x=0;
+        }
+    }
+
+    fclose(f);
+    return x;
+}
+
+// FUNÇÕES ATUALIZAR REGISTRO
+int alterar_videogame(struct videogame v) { // OK
+    struct videogame v2;
+    FILE *f = fopen("arquivos/videogame.bin", "r+b");
+    int x=1;
+    while(fread(&v2, sizeof(struct videogame), 1, f)) {
+        if(v.id == v2.id) {
+            fseek(f, v.id*sizeof(struct videogame), 0);
+            fwrite(&v, sizeof(struct videogame), 1, f);
+            x=0;
+            break;
+        }
+    }
+
+    fclose(f);
+    return x;
+}
+int alterar_jogo(struct jogo j) { // OK
+    struct jogo j2;
+    FILE *f = fopen("arquivos/jogo.bin", "r+b");
+    int x=1;
+    while(fread(&j2, sizeof(struct jogo), 1, f)) {
+        if(j.id == j2.id) {
+            fseek(f, j.id*sizeof(struct jogo), 0);
+            fwrite(&j, sizeof(struct jogo), 1, f);
+            x=0;
+            break;
+        }
+    }
+
+    fclose(f);
+    return x;
+}
+int alterar_pessoa(struct pessoa p) { // OK
+    struct pessoa p2;
+    FILE *f = fopen("arquivos/pessoa.bin", "r+b");
+    int x=1;
+    while(fread(&p2, sizeof(struct pessoa), 1, f)) {
+        if(p.id == p2.id) {
+            fseek(f, p.id*sizeof(struct pessoa), 0);
+            fwrite(&p, sizeof(struct pessoa), 1, f);
+            x=0;
+            break;
+        }
+    }
+
+    fclose(f);
+    return x;
+}
+int alterar_venda(struct venda v, struct venda w) { // OK
+    struct venda v2;
+    FILE *f = fopen("arquivos/venda.bin", "r+b");
+    int x=1, verificacao;
+    while(fread(&v2, sizeof(struct venda), 1, f)) {
+        verificacao = strcmp(v.data, v2.data);
+        if(v.pessoa_id == v2.pessoa_id && v.jogo_id == v2.jogo_id && verificacao == 0) {
+            fseek(f, -1*sizeof(struct venda), SEEK_CUR);
+            fwrite(&w, sizeof(struct venda), 1, f);
+            x=0;
+            break;
         }
     }
 
